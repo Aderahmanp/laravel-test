961c7cebd7dcf409222acdcb547b70564922bfef
aderahmanp <ade.rahmanp@alfabeta.co.id>
2019-12-12 15:05:22 +0700

    develop

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -24,7 +24,7 @@ class TestController extends Controller
         //     dump($t->startup);
         //  }
         
-        //  dd($employee);
+        //  dd($employee)
 
         return view ('test', compact ('startup'));
 
