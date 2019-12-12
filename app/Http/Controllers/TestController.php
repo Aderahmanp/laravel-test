fb0fe2ec50c63613617fed37f5fba490a1b99559
yusuffaizdev <yusuf.faisal@alfabeta.id>
2019-12-12 15:07:41 +0700

    ade

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -2,34 +2,15 @@
 
 namespace App\Http\Controllers;
 
-use App\Startup;
 use App\Employee;
-use Illuminate\Http\Request;
 
 class TestController extends Controller
 {
-    public function index() {
-        $startup = Startup::with('employees')->get();
-        // $employee = Employee::with('startup')->get();
+    public function index()
+    {
+        $employees = Employee::with('startup')->get();
 
-        // foreach ($startup as $t) {
-        
-        //    dd($t->employees);
-        // }
-       
-        // dd($startup);
-
-        // foreach ($employee as $t) {
-        
-        //     dump($t->startup);
-        //  }
-        
-        //  dd($employee)
-
-        return view ('test', compact ('startup'));
-
-       
-
+        return view('test', compact('employees'));
     }
 }
 
